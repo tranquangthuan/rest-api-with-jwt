9aa59c84d04ff668252a8dbaad8ed7baf865a924
Thuan Tran <thuan.uit@gmail.com>
2023-09-02 11:38:13 +0700

    update coding convention

--- src/main/java/com/fa/restapiwithjwt/service/JwtService.java
+++ src/main/java/com/fa/restapiwithjwt/service/JwtService.java
@@ -19,13 +19,13 @@ public class JwtService {
 
 	public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
 
-	public String generateToken(String userName) {
+	public String generateToken(String username) {
 		Map<String, Object> claims = new HashMap<>();
-		return createToken(claims, userName);
+		return createToken(claims, username);
 	}
 
-	private String createToken(Map<String, Object> claims, String userName) {
-		return Jwts.builder().setClaims(claims).setSubject(userName).setIssuedAt(new Date(System.currentTimeMillis()))
+	private String createToken(Map<String, Object> claims, String username) {
+		return Jwts.builder().setClaims(claims).setSubject(username).setIssuedAt(new Date(System.currentTimeMillis()))
 				.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30))
 				.signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
 	}
